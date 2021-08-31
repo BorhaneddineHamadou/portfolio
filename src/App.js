6adf957909a9c6a2b5d54ead0b42c720e9198c8d
Borhaneddine <haborhaneddine@gmail.com>
2021-08-31 20:22:40 +0100

    Header Component Fixing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Header from './components/HeaderComponent';
 import Navbar from './components/NavbarComponent';
+import Particles from 'react-particles-js';
 
 function App() {
   return (
-     <Navbar />
+    <>
+       <Particles className="particles-canvas" params={{
+         particles: {
+             number: {
+               value: 30,
+               density: {
+                 enable:true,
+                 value_area: 900
+               }
+             },
+             shape:{
+               type: 'circle',
+               stroke: {
+                 width: 6,
+                 color: "#05F6E7"
+               }
+             }
+         }
+       }} />
+       <Navbar />
+       <Header />
+    </>
   );
 }
 
---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Typed from 'react-typed';
+
+const Header = () => {
+    return (
+        <div className="header-wraper">
+            <div className="main-info">
+                <h1>Web development and website promotions</h1>
+                <Typed className="typed-text" strings={[
+                    "Mobile Development", "Web Development", "Data Science", "Machine Learning"
+                ]}
+                typeSpeed={40}
+                backSpeed={60}
+                loop />
+                <a href="" className="btn-main-offer">Contact Me</a>
+            </div>
+        </div>
+    )
+}
+
+export default Header
